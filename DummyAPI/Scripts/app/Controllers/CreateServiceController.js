10400285f7f272e0f503561158adc4ac6d03912f
vandeman <padraic.duffy.1@gmail.com>
2014-05-10 18:48:52 +0100

    Created form to create a web service, managed by angular framework

---
+++ DummyAPI/Scripts/app/Controllers/CreateServiceController.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="~/scripts/angular.js" />
+/// <reference path="../app.js" />
+
+'use strict';
+
+app.controller('CreateServiceController',
+    function CreateServiceController($scope) {
+
+        $scope.service = {
+            responseBody: 'testData',
+            url: ''
+        },
+        $scope.createService = function(service)
+        {
+            service.url = 'http://google.com';
+            //window.alert(service.responseBody);
+        },
+        $scope.clearService = function (service)
+        {
+            service.responseBody = '';
+            service.url = '';
+        }
+
+
+    });
+
--- DummyAPI/Scripts/app/app.js
+++ DummyAPI/Scripts/app/app.js
@@ -1,3 +1,4 @@
 ﻿'use strict';
 
 var app = angular.module('DummyAPI', []);
+
